19d465f2cf64828353ce45c93e1053df8a414d9a
ismailoviso <77150917+ismailoviso@users.noreply.github.com>
2021-07-28 23:14:50 +0500

    Program added

---
+++ script.js
@@ -0,0 +1,80 @@
+var d = +prompt('Введите время');
+
+switch (d) {
+    case 1:
+        alert(' 1 часов ночи');
+        break;
+    case 2:
+        alert('2 часа ночи');
+        break;
+    case 3:
+        alert('3 часа ночи');
+        break;
+    case 4:
+        alert('4 часа утра');
+        break;
+    case 5:
+        alert('5 часов утра');
+        break;
+    case 6:
+        alert('6 часов утра ');
+        break;
+    case 7:
+        alert('7 часов утра');
+        break;
+    case 8:
+        alert('8 часов утра');
+        break;
+    case 9:
+        alert('9 часов утра');
+        break;
+    case 10:
+        alert('10 часов утра');
+        break;
+    case 11:
+        alert('11 часов утра');
+        break;
+    case 12:
+        alert('12 часов дня Полдень');
+        break;
+    case 13:
+        alert('1 час дня полудни :)');
+        break;
+    case 14:
+        alert('2 часа дня');
+        break;
+    case 15:
+        alert('3 часа дня');
+        break;
+    case 16:
+        alert('4 часа дня');
+        break;
+    case 17:
+        alert('5 часов вечера');
+        break;
+    case 18:
+        alert('6 часов вечера');
+        break;
+    case 19:
+        alert('7 часов вечера');
+        break;
+    case 20:
+        alert('8 часов вечера');
+        break;
+    case 21:
+        alert('9 часов вечера');
+        break;
+    case 22:
+        alert('10 часов вечера');
+        break;
+    case 23:
+        alert('11 часов вечера');
+        break;
+    case 24:
+        alert('0:00 полночь');
+        break;
+
+    default:
+        alert('ты шо наделал даун!!!');
+        break;
+}
